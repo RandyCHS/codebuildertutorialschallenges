7be6ea298b27aaee3a6ac875a59d81c652e794fb
Randy Macdonald <41399033+RandyCHS@users.noreply.github.com>
2023-01-21 20:38:11 -0800

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,31 @@
+
+> Open this page at [https://randychs.github.io/codebuildertutorialschallenges/](https://randychs.github.io/codebuildertutorialschallenges/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://minecraft.makecode.com/](https://minecraft.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/randychs/codebuildertutorialschallenges** and import
+
+## Edit this project ![Build status badge](https://github.com/randychs/codebuildertutorialschallenges/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://minecraft.makecode.com/](https://minecraft.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/randychs/codebuildertutorialschallenges** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/randychs/codebuildertutorialschallenges/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/minecraft
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,180 @@
+player.onChat("Challenge3", function () {
+    xPos = 192
+    yPos = 4
+    zPos = 7
+    agent.teleport(world(xPos, yPos, zPos), WEST)
+    numFwd = 3
+    agent.move(FORWARD, numFwd)
+    numUp = 1
+    agent.move(UP, numUp)
+    numFwd = 3
+    agent.move(FORWARD, numFwd)
+    for (let index = 0; index < 2; index++) {
+        numUp = 1
+        agent.move(UP, numUp)
+        numFwd = 1
+        agent.move(FORWARD, numFwd)
+    }
+    numFwd = 1
+    agent.move(FORWARD, numFwd)
+    numDn = 3
+    agent.move(DOWN, numDn)
+    numFwd = 3
+    agent.move(FORWARD, numFwd)
+    numUp = 1
+    agent.move(UP, numUp)
+    numFwd = 2
+    agent.move(FORWARD, numFwd)
+    numDn = 1
+    agent.move(DOWN, numDn)
+    numUp = 1
+    agent.move(UP, numUp)
+    numFwd = 3
+    agent.move(FORWARD, numFwd)
+    numDn = 2
+    agent.move(DOWN, numDn)
+    numFwd = 3
+    agent.move(FORWARD, numFwd)
+})
+player.onChat("fwdNum", function (xPos) {
+    agent.move(FORWARD, xPos)
+})
+player.onChat("Challenge2", function () {
+    xPos = 208
+    yPos = 4
+    zPos = 7
+    agent.teleport(world(xPos, yPos, zPos), WEST)
+    numFwd = 7
+    agent.move(FORWARD, numFwd)
+    agent.turn(RIGHT_TURN)
+    numFwd = 3
+    agent.move(FORWARD, numFwd)
+})
+player.onChat("compass", function () {
+    p = player.position()
+    east = positions.add(
+    p,
+    pos(length, 0, 0)
+    )
+    blocks.fill(
+    OBSIDIAN,
+    p,
+    east,
+    FillOperation.Replace
+    )
+    blocks.print(
+    "EAST+X",
+    REDSTONE_BLOCK,
+    east,
+    EAST
+    )
+    west = positions.add(
+    p,
+    pos(0 - length, 0, 0)
+    )
+    blocks.fill(
+    OBSIDIAN,
+    p,
+    west,
+    FillOperation.Replace
+    )
+    blocks.print(
+    "WEST-X",
+    REDSTONE_BLOCK,
+    positions.add(
+    west,
+    pos(-30, 0, 0)
+    ),
+    EAST
+    )
+    up = positions.add(
+    p,
+    pos(0, length, 0)
+    )
+    blocks.fill(
+    OBSIDIAN,
+    p,
+    up,
+    FillOperation.Replace
+    )
+    blocks.print(
+    "UP+Y",
+    REDSTONE_BLOCK,
+    up,
+    EAST
+    )
+    down = positions.add(
+    p,
+    pos(0, 0 - length, 0)
+    )
+    blocks.fill(
+    OBSIDIAN,
+    p,
+    down,
+    FillOperation.Replace
+    )
+    blocks.print(
+    "DOWN-Y",
+    REDSTONE_BLOCK,
+    down,
+    EAST
+    )
+    north = positions.add(
+    p,
+    pos(0, 0, 0 - length)
+    )
+    blocks.fill(
+    OBSIDIAN,
+    p,
+    north,
+    FillOperation.Replace
+    )
+    blocks.print(
+    "NORTH-Z",
+    REDSTONE_BLOCK,
+    north,
+    EAST
+    )
+    south = positions.add(
+    p,
+    pos(0, 0, length)
+    )
+    blocks.fill(
+    OBSIDIAN,
+    p,
+    south,
+    FillOperation.Replace
+    )
+    blocks.print(
+    "SOUTH+Z",
+    REDSTONE_BLOCK,
+    south,
+    EAST
+    )
+})
+player.onChat("tpWdPsW", function (xPos, yPos, zPos) {
+    agent.teleport(world(xPos, yPos, zPos), WEST)
+})
+player.onChat("Challenge1", function () {
+    xPos = 229
+    yPos = 4
+    zPos = 7
+    agent.teleport(world(xPos, yPos, zPos), WEST)
+    numFwd = 10
+    agent.move(FORWARD, numFwd)
+})
+let south: Position = null
+let north: Position = null
+let down: Position = null
+let up: Position = null
+let west: Position = null
+let east: Position = null
+let p: Position = null
+let numDn = 0
+let numUp = 0
+let numFwd = 0
+let zPos = 0
+let yPos = 0
+let xPos = 0
+let length = 0
+length = 25
